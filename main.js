07f4f941af9c14d0d2a5be6f8fe4bcb8afc7567f
SlawomirDudziak <slaweq09@gmail.com>
2020-06-07 18:59:36 +0200

    Addition + changed lcd to input

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-
-//var LCD=String(document.getElementById("LCD"));
 var LCD='';
+var firstNumber;
+var secondNumber=0;
+var score=0;
 
 function select(digit)
 {
 	LCD=digit;
-	//document.getElementById("LCD").innerHTML= digit;
-	document.getElementById("LCD").innerHTML+= LCD;
+	//document.getElementById("LCD").innerHTML+= digit;
+	document.getElementById("LCD").value+=LCD;
+	return LCD;
 }
-function clear()
+function clearScreen()
 {
-	var LCD='';
-	var LCDdiv=document.getElementById("LCD");
-	LCDdiv.textContent=0;
+	document.getElementById("LCD").value='';
+	//console.log('2');
+}
+function addition()
+{
+	firstNumber=document.getElementById("LCD").value;
+	var firstNumber = parseInt(firstNumber);
+	//firstNumber=String(firstNumber);
+	console.log(firstNumber, typeof(firstNumber));
+	clearScreen();
+	//console.trace()
+	score += firstNumber;
+}
+function equals()
+{
+	console.log(score, 'score');
+	secondNumber=parseInt(document.getElementById("LCD").value);
+	score+=secondNumber;
+	console.log(score, 'score');
+	document.getElementById("LCD").value=score;
+	score=0;
 }
